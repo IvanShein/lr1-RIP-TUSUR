c74bb61680b27aa39fbf2b5cc67fa74c05c50a2a
Ivan Shein <ivan-shein-yar@yandex.ru>
2024-01-22 01:49:10 +0700

    fix: исправлена ошибка рботы роута POST

--- src/Controller/FaqController.php
+++ src/Controller/FaqController.php
@@ -16,26 +16,34 @@ class FaqController extends AbstractController
     public function getAllFaqs(FaqRepository $repo): JsonResponse
     {
         $allFaqs = $repo->findAll();
-    
+
         return $this->json([
             'message' => 'This is all FAQs.',
             'path' => $allFaqs,
         ]);
     }
-    
+
     #[Route('/', name: 'create_faq', methods: 'POST')]
     public function createFaq(Request $request, FaqRepository $repo): JsonResponse
     {
-        if(is_null($request->request->get(key: 'question', default: null))) return $this->json([
+        // if(is_null($request->request->get(key: 'question', default: null))) return $this->json([
+        // if(is_null($request->request->get('question', null))) return $this->json([
+        //     'message' => 'No data. The field -question- is required.'
+        // ], status: 400);
+
+        $data = json_decode($request->getContent(), associative:true);
+
+        if(!$data['question']) return $this->json([
             'message' => 'No data. The field -question- is required.'
         ], status: 400);
 
         $faq = new Faq();
-        $faq->setQuestion($request->request->get(key: 'question'));
-        $faq->setAnswer($request->request->get(key: 'answer'));
-    
+        // $faq->setQuestion($request->request->get(key: 'question'));
+        $faq->setQuestion($data['question']);
+        $faq->setAnswer($data['answer']);
+
         $repo->save($faq, true);
-    
+
         return $this->json([
             'message' => 'Faq was created successfully',
             'path' => $faq,
@@ -58,9 +66,9 @@ class FaqController extends AbstractController
         if(array_key_exists('answer', $data) && $data['answer'] !== $faq->getAnswer()) {
             $faq->setAnswer($data['answer']);
         };
-    
+
         $repo->save($faq, true);
-    
+
         return $this->json([
             'message' => 'Faq was edited successfully',
             'path' => $faq,
@@ -74,9 +82,9 @@ class FaqController extends AbstractController
         if(!$faq instanceof Faq) return $this->json([
             'message' => 'Question with this ID have not be found.'
         ], status: 404);
-    
+
         $repo->remove($faq, true);
-    
+
         return $this->json([
             'message' => 'Faq was deleted successfully',
             'path' => $faq,
